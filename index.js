447829bea98ab2432f5324e240532b2674797297
Jaydeep-93 <jaydeepkachare123@gmail.com>
2021-11-28 11:42:45 +0530

    namespaced (scoped) package

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function log(message) {
     if (message) 
-        console.log(`@jdlogger :     ${message}`);
+        console.log(`@jd/jd-logger :     ${message}`);
 }
 
 module.exports = log ; ``
